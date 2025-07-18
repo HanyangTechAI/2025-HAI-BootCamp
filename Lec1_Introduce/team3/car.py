437415803fe12bd2d5091184af2fa427b117bcce
asdf178 <rlatjddmscoding@gamil.com>
2025-07-18 14:50:13 +0900

    feat: add car

---
+++ Lec1_Introduce/team3/car.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self, registrationNum: int, year: int, licenseNumber: str):
+        self.registrationNum = registrationNum
+        self.year = year
+        self.licenseNumber = licenseNumber
+    
+    def moveForward():
+        print("moveForward")
+    
+    def moveBackward():
+        print("moveBackward")
+    
+    def stop():
+        print("stop")
+    
+    def turnRight():
+        print("turn right")
+    
+    def turnLeft():
+        print("turn left")
+
