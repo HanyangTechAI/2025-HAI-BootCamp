a80afb6d8069a1d6e0dac27c641e453c24863510
noisol <opendy1217@gmail.com>
2025-07-18 14:51:18 +0900

    feat: make class Car

---
+++ Lec1_Introduce/team2/car.py
@@ -0,0 +1,5 @@
+class Car:
+    def __init__(self, registrationNum, year, licenseNumber):
+        self.registrationNum = registrationNum
+        self.year = year
+        self.licenseNumber = licenseNumber
