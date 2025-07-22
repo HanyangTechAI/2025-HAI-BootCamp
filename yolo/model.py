986cf6b64e28c0f88b0b505e930ef24415e322e7
noisol <opendy1217@gmail.com>
2025-07-23 03:11:48 +0900

    feat: make darknet

---
+++ yolo/model.py
@@ -0,0 +1,82 @@
+import torch.nn as nn
+
+# hyper parameter
+S = 7 # 결과물의 width, height
+B = 5 # 5개의 값 : 중심 좌표 x, y | width, height | 물체가 있을 확률
+C = 20 # 클래스 20개 -> 구분할 수 있는 object가 20개
+
+class YOLO(nn.Module):
+    def __init__(self, init_weight=True):
+        super(YOLO, self).__init__()
+        if init_weight:
+            self._initialize_weights()
+
+        self.darknet = self._make_conv_layers() # darknet : YOLO의 fully connected layer 전 모든 conv layer
+        self.fully_connected_layer = self._make_fc_layers()
+
+    def forward(self, x):
+        x = self.darknet(x)
+        x = self.fully_connected_layer(x)
+        return x
+    
+    def _make_conv_layers():
+        darknet = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, padding=3, stride=2),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(kernel_size=2, padding=0, stride=2),
+
+            nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(kernel_size=2, padding=0, stride=2),
+
+            nn.Conv2d(in_channels=192, out_channels=128, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(kernel_size=2, padding=0, stride=2),
+
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),    
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(kernel_size=2, padding=0, stride=2),
+
+            nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1, padding=0, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1, stride=2),
+            nn.LeakyReLU(0.1, inplace=True),
+
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        return darknet
