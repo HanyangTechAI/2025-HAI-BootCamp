35209a0fa43805e8de36223348e57c6d8d04814e
BeomJun <kbj1418@naver.com>
2025-07-23 14:35:49 +0900

    test

---
+++ yolo/model.py
@@ -0,0 +1,138 @@
+import torch.nn as nn
+
+S = 7 
+B = 2
+C = 20
+
+class YOLO(nn.Module):
+    def __init__(self, init_weight=True):
+        super(YOLO, self).__init__()
+        if init_weight:
+            self._initialize_weights()
+
+        self.darknet = self._make_conv_layers()
+        self.fully_connected_layer = self._make_fc_layers()
+
+    def forward(self, x):
+        x = self.darknet(x)
+        x = self.fully_connected_layer(x)
+        return x
+
+    def _make_conv_layers(self): 
+        darknet = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            
+            nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            
+            nn.Conv2d(in_channels=192, out_channels=128, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+        
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 192, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(192, 128, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+
+            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+
+            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+
+            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+
+            nn.Conv2d(512, 512, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(1024, 512, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+
+            nn.Conv2d(1024, 512, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=2, padding=1),
+
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        return darknet
+        
+    def _make_fc_layers(self):
+        fc = nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(1024 * 7 * 7, 4096),
+        nn.LeakyReLU(0.1),
+        nn.Dropout(0.5),
+        nn.Linear(4096, S * S * (B * 5 + C)) 
+        )
+        return fc
+        
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
